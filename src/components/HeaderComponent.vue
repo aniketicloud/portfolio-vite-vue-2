c3bd941319a9bd5bdccb6bf92ffd46f94cd433b1
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-09-18 14:02:38 +0530

    setting header content

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,18 +1,24 @@
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/code.svg"
-      width="125"
-      height="125"
-    />
+    <div class="logo">
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/code.svg"
+        width="125"
+        height="125"
+      />
+      <h1>Nikefy</h1>
+    </div>
 
-    <div class="wrapper">
+    <div class="route-links">
       <nav>
         <router-link to="/">Home</router-link>
+        <router-link to="/projects">Projects</router-link>
         <router-link to="/about">About</router-link>
       </nav>
     </div>
   </header>
 </template>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const router = new VueRouter({
       component: HomeView
     },
     {
+      path: '/projects',
+      name: 'projects',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
