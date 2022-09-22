98d09ed3e082ee5a5c09db4c3b7fbf1c08f0bf09
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-09-22 21:56:51 +0530

    font for header, nav-links with hover effect

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -8,15 +8,72 @@
         width="48"
         height="48"
       />
-      <h2>Nikefy</h2>
+      <h1>Nikefy</h1>
     </div>
 
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/projects">Projects</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link class="nav-link" data-item="Home" to="/">Home</router-link>
+      <router-link class="nav-link" data-item="Projects" to="/projects"
+        >Projects</router-link
+      >
+      <router-link class="nav-link" data-item="About" to="/about"
+        >About</router-link
+      >
     </nav>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo {
+  display: grid;
+  grid: auto-flow/1fr 1fr;
+  align-items: center;
+  gap: 0.5em;
+  margin-top: 0.125em;
+
+  img {
+    justify-self: end;
+  }
+
+  h1 {
+    font-family: "Bookerly Bold";
+  }
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 0.75em;
+
+  .nav-link {
+    text-decoration: none;
+    color: #8f8f8f;
+    font-family: "Bookerly";
+    font-size: 24px;
+    transition: all 0.5s ease-in-out;
+    position: relative;
+    text-transform: uppercase;
+
+    &::before {
+      content: attr(data-item);
+      transition: 0.5s;
+      color: #8254ff;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      width: 0;
+      overflow: hidden;
+      
+    }
+
+    &:hover {
+      &::before {
+        width: 100%;
+        transition: all 0.5s  ease-in-out;
+      }
+    }
+  }
+}
+</style>
